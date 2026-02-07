1a7f3847459747120345de3e8bcba75f96fc8337
Shrushti-Kusale <shrushtikusale@gmail.com>
2026-02-07 23:21:26 +0530

    --Project Header File

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php include "db.php"; ?>
+<link rel="stylesheet" href="style.css">
+
+<div class="sidebar">
+<h3 style="color:white;text-align:center;">Carbon App</h3>
+<a href="home.php">Home</a>
+<a href="calculator_start.php">Calculator</a>
+<a href="dashboard.php">Dashboard</a>
+<a href="compare.php">Comparison</a>
+<a href="contact.php">Feedback</a>
+<a href="logout.php">Logout</a>
+</div>
+
+<div class="main">
